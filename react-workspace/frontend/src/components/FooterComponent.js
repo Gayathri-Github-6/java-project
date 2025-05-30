d3a08adb79aa16ee06cbf901de9b32110bbf8516
THALLAPALLI GAYATHRI <tgayathri9912@gmail.com>
2025-05-30 14:55:36 +0530

    frontend code

---
+++ react-workspace/frontend/src/components/FooterComponent.js
@@ -0,0 +1,10 @@
+import '../App.css';
+function FooterComponent()
+{
+    return(
+        <footer className="footer">
+            <small>All Right Reserved &copy; by Gayathri, 2025</small>
+        </footer>
+    )
+}
+export default FooterComponent;
